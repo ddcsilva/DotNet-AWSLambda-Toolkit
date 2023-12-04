687c562bbcbdd8338c2c2c908c4978fbad3c61fc
danilo.silva <danilo.silva@msn.com>
2023-12-04 09:03:54 -0300

    Salvar e Consultar dados no DynamoDB a partir do .NET

--- src/Danilo.Udemy.Lambda/Danilo.Udemy.Lambda/Function.cs
+++ src/Danilo.Udemy.Lambda/Danilo.Udemy.Lambda/Function.cs
@@ -1,27 +1,37 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+// Atributo Assembly para habilitar a entrada JSON da função Lambda para ser convertida em uma classe .NET.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace Danilo.Udemy.Lambda;
 
 public class Function
 {
-    
+
     /// <summary>
-    /// A simple function that takes a string and does a ToUpper
+    /// Função Lambda para retornar um usuário do DynamoDB.
     /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
+    /// <param name="input">Id do usuário</param>
+    /// <param name="context">Contexto da função Lambda</param>
     /// <returns></returns>
-    public string FunctionHandler(Usuario input, ILambdaContext context)
+    public async Task<Usuarios> FunctionHandler(Guid input, ILambdaContext context)
     {
-        return input.Nome.ToUpper();
+        // Instância do DynamoDBContext para realizar a consulta no DynamoDB.
+        var dynamoDBContext = new DynamoDBContext(new AmazonDynamoDBClient());
+        // Consulta no DynamoDB, passando o Id do usuário como parâmetro.
+        var usuario = await dynamoDBContext.LoadAsync<Usuarios>(input);
+
+        return usuario;
     }
 }
 
-public class Usuario
+public class Usuarios
 {
+    // Atributo para definir a chave primária da tabela.
+    // DynamoDBHashKey: Chave primária do tipo hash.
+    [DynamoDBHashKey]
     public Guid Id { get; set; }
     public string Nome { get; set; }
 }
